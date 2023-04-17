bca855f60de71d5e553d5899f05765214ffd1f05
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-04-17 15:00:29 -0400

    Created Tables

---
+++ database/migrations/2023_04_17_183402_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('body');
+            $table->binary('image')->nullable();
+            $table->timestamps();
+            $table->integer('receivers_id')->unsigned();
+            $table->integer('senders_id')->unsigned();
+
+            $table->foreign('receivers_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onCascade('delete');
+
+            $table->foreign('senders_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
