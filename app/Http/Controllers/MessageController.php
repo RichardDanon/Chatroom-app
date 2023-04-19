a027cfc7d469636d49556a989f260e701faee27f
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-04-19 17:47:22 -0400

    Message controller change 1

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,61 +16,55 @@
 
     class ChatUserController extends Controller
     {
-        public function setItem(Request $request){
-            $fields = $request->validate([
-                'name'          => 'required|string',
-                'description'   => 'required|string',
-                'price'         => 'required|decimal:2',//there is not validate double
-                'image'         => 'image|nullable|max:1999',
-                'category_id'   => 'required|integer',
+        public function index()
+        {
+            $messages = Message::all();
+            return view('messages.index', compact('messages'));
+        }
+    
+        public function create()
+        {
+            return view('messages.create');
+        }
+    
+        public function store(Request $request)
+        {
+            $validatedData = $request->validate([
+                'body' => 'required|string',
+                'image' => 'nullable|image',
+                'receivers_id' => 'required|exists:chatUsers,id',
+                'senders_id' => 'required|exists:chatUsers,id',
             ]);
-           $item = Item::create([
-                'name'        => $fields['name'],
-                'description' => $fields['description'],
-                'price'       => $fields['price'],
-                //'image'=> $fields['image']:null,
-                'category_id'  => $fields['category_id']
+    
+            Message::create($validatedData);
+    
+            return redirect()->route('messages.index')->with('success', 'Message created successfully.');
+        }
+    
+        public function edit(Message $message)
+        {
+            return view('messages.edit', compact('message'));
+        }
+    
+        public function update(Request $request, Message $message)
+        {
+            $validatedData = $request->validate([
+                'body' => 'required|string',
+                'image' => 'nullable|image',
+                'receivers_id' => 'required|exists:chatUsers,id',
+                'senders_id' => 'required|exists:chatUsers,id',
             ]);
     
-            return response($item, 201);
+            $message->update($validatedData);
+    
+            return redirect()->route('messages.index')->with('success', 'Message updated successfully.');
         }
     
-        public function getItems(){
-            $arryItems = Item::all();
-            return response($arryItems, 201);
-        }
+        public function destroy(Message $message)
+        {
+            $message->delete();
     
-        public function getItem($search){
-            $arryItems = Item::where('name', 'LIKE', '%'.$search.'%')
-                            ->orWhere('description', 'LIKE', '%'.$search.'%')
-                            ->get();
-            return response($arryItems, 201);
-        }
-    
-        public function getItemsByCategory($search){
-            $categories = Category::where('name', 'LIKE', '%'.$search.'%')->get();
-            $items=[];
-            if(sizeof($categories)>0){
-                $idCat = $categories[0]->id;
-                $items = Category::find($idCat)->items;
-            }
-    
-            foreach ($items as $item) {
-                // ...
-            }
-            return response($items, 201);
-        }
-    
-        public function getCategoryByItem($search){
-            $items = Item::where('name', 'LIKE', '%'.$search.'%')
-                            ->orWhere('description', 'LIKE', '%'.$search.'%')
-                            ->get();
-            if(isset($items) && sizeof($items)>0){
-                $idItem= $items[0]->id;
-                $category = Item::find($idItem)->category;
-                return response([$category], 201);
-            }
-            return response([],201);
+            return redirect()->route('messages.index')->with('success', 'Message deleted successfully.');
         }
     }
 ?>
