154322dfb84331a0b5968f320f81901d607b0682
Caleb Liu <2130904@champlaincollege.qc.ca>
2023-04-19 16:43:37 -0400

    controller URIs for chat user and message

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/ext/setChatUser',[PageController::class,'setChatUser']);
+Route::get('/ext/getChatUser',[PageController::class,'getChatUser']);
+Route::update('/ext/putChatUser',[PageController::class,'putChatUser']);
+
+Route::post('/ext/setMessage',[PageController::class,'setMessage']);
+Route::get('/ext/getMessage',[PageController::class,'getMessage']);
+Route::update('/ext/putMessage',[PageController::class,'putMessage']);
+Route::delete('/ext/deleteMessage',[PageController::class,'deleteMessage']);
+
+
