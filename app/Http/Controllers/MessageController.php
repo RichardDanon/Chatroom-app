db48c2cca734bb6fbd68adfffd6d1ffa7712697b
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-04-24 15:27:15 -0400

    Revert "Merge branch 'master' of https://github.com/RichardDanon/Chatroom-app"
    This reverts commit c9b7375d9c3dd22f151b4ae2924db32bd3e9a6f5, reversing
    changes made to 1941036564c3e38832e1b712a7fa2604237bf95d.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,55 +16,61 @@
 
     class ChatUserController extends Controller
     {
-        public function index()
-        {
-            $messages = Message::all();
-            return view('messages.index', compact('messages'));
-        }
-    
-        public function create()
-        {
-            return view('messages.create');
-        }
-    
-        public function store(Request $request)
-        {
-            $validatedData = $request->validate([
-                'body' => 'required|string',
-                'image' => 'nullable|image',
-                'receivers_id' => 'required|exists:chatUsers,id',
-                'senders_id' => 'required|exists:chatUsers,id',
+        public function setItem(Request $request){
+            $fields = $request->validate([
+                'name'          => 'required|string',
+                'description'   => 'required|string',
+                'price'         => 'required|decimal:2',//there is not validate double
+                'image'         => 'image|nullable|max:1999',
+                'category_id'   => 'required|integer',
+            ]);
+           $item = Item::create([
+                'name'        => $fields['name'],
+                'description' => $fields['description'],
+                'price'       => $fields['price'],
+                //'image'=> $fields['image']:null,
+                'category_id'  => $fields['category_id']
             ]);
     
-            Message::create($validatedData);
-    
-            return redirect()->route('messages.index')->with('success', 'Message created successfully.');
+            return response($item, 201);
         }
     
-        public function edit(Message $message)
-        {
-            return view('messages.edit', compact('message'));
+        public function getItems(){
+            $arryItems = Item::all();
+            return response($arryItems, 201);
         }
     
-        public function update(Request $request, Message $message)
-        {
-            $validatedData = $request->validate([
-                'body' => 'required|string',
-                'image' => 'nullable|image',
-                'receivers_id' => 'required|exists:chatUsers,id',
-                'senders_id' => 'required|exists:chatUsers,id',
-            ]);
-    
-            $message->update($validatedData);
-    
-            return redirect()->route('messages.index')->with('success', 'Message updated successfully.');
+        public function getItem($search){
+            $arryItems = Item::where('name', 'LIKE', '%'.$search.'%')
+                            ->orWhere('description', 'LIKE', '%'.$search.'%')
+                            ->get();
+            return response($arryItems, 201);
         }
     
-        public function destroy(Message $message)
-        {
-            $message->delete();
+        public function getItemsByCategory($search){
+            $categories = Category::where('name', 'LIKE', '%'.$search.'%')->get();
+            $items=[];
+            if(sizeof($categories)>0){
+                $idCat = $categories[0]->id;
+                $items = Category::find($idCat)->items;
+            }
     
-            return redirect()->route('messages.index')->with('success', 'Message deleted successfully.');
+            foreach ($items as $item) {
+                // ...
+            }
+            return response($items, 201);
+        }
+    
+        public function getCategoryByItem($search){
+            $items = Item::where('name', 'LIKE', '%'.$search.'%')
+                            ->orWhere('description', 'LIKE', '%'.$search.'%')
+                            ->get();
+            if(isset($items) && sizeof($items)>0){
+                $idItem= $items[0]->id;
+                $category = Item::find($idItem)->category;
+                return response([$category], 201);
+            }
+            return response([],201);
         }
     }
 ?>
