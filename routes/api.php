144e8fc862ad989532de99f1020b48ea81a4163c
MaximG01 <Maxamazing96@gmail.com>
2023-04-26 14:19:39 -0400

    apis dont work ;(
    changed up some stuff, apis didnt work b4, tried to fix em and they still dont work. Help

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatUserController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +20,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/ext/setChatUser',[PageController::class,'setChatUser']);
-Route::get('/ext/getChatUser',[PageController::class,'getChatUser']);
-Route::put('/ext/putChatUser',[PageController::class,'putChatUser']);
-Route::delete('/ext/deleteChatUser',[PageController::class,'deleteChatUser']);
+Route::post('/ext/setChatUser',[ChatUserController::class,'setChatUser']);
+Route::get('/ext/getChatUser',[ChatUserController::class,'getChatUser']);
+Route::put('/ext/putChatUser',[ChatUserController::class,'putChatUser']);
+Route::delete('/ext/deleteChatUser',[ChatUserController::class,'deleteChatUser']);
 
 
-Route::post('/ext/setMessage',[PageController::class,'setMessage']);
-Route::get('/ext/getMessage',[PageController::class,'getMessage']);
+Route::post('/ext/setMessage',[MessageController::class,'setMessage']);
+Route::get('/ext/getMessage',[MessageController::class,'getMessage']);
+Route::get('/ext/getMessage/{searchId}', [MessageController::class, 'getMessageById']);
 //Route::update('/ext/putMessage',[PageController::class,'putMessage']);
 //Route::delete('/ext/deleteMessage',[PageController::class,'deleteMessage']);
 
