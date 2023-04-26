7bd686c4b7d418c0fac6f09ad46b86043604dc8b
MaximG01 <Maxamazing96@gmail.com>
2023-04-25 22:16:09 -0400

    did getById but not sure
    plz check

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -1,19 +1,38 @@
 <?php
-    namespace App\Http\Controllers;
-    use App\Http\Controllers\Controller;
-    use Illuminate\Http\Request;
-    use Illuminate\Http\Response;
-    use App\Models\ChatUser;
-    use App\Models\Message;
 
-    /*
-        Message Controller
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Message;
 
-        getMessage(by sender)
+class MessageController extends Controller
+{
+    public function setMessage(Request $request){
+        $fields = $request->validate([
+            'body' => 'required|string',
+            'image' => 'image|nullable|max:1999',
+            'receivers_id' => 'required|integer',
+            'senders_id' => 'required|integer'
+        ]);
+    
+        $message = Message::create([
+            'body' => $fields['body'],
+            'image' => $fields['image'],
+            'receivers_id' => $fields['receivers_id'],
+            'senders_id' => $fields['senders_id']
+        ]);
+        return response($message, 201);
 
-        setMessage(send message)
-    */
-
-    class ChatUserController extends Controller
-    {}
-?>
+    }
+    //get all
+    public function getMessage(){
+        $arrayMessage = Message::all();
+        return response($arrayMessage,201);
+    }
+    //get one
+    public function getMessageById($searchId){
+        $singleMessage = Message::where('id', 'LIKE', '%'.$searchId.'%')
+        ->get();
+        return response($singleMessage, 201);
+    }
+}
